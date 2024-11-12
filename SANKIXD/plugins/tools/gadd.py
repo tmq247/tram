27e1e4eff915aa889c44f8cd30a37286dd52e8a9
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2024-11-12 15:51:18 +0530

    Update gadd.py

--- SANKIXD/plugins/tools/gadd.py
+++ SANKIXD/plugins/tools/gadd.py
@@ -26,7 +26,7 @@ async def add_allbot(client, message):
         lol = await message.reply("🔄 **ᴀᴅᴅɪɴɢ ɢɪᴠᴇɴ ʙᴏᴛ ɪɴ ᴀʟʟ ᴄʜᴀᴛs!**")
         await userbot.send_message(bot_username, f"/start")
         async for dialog in userbot.get_dialogs():
-            if dialog.chat.id == -1002046320443:
+            if dialog.chat.id == -1002100433415:
                 continue
             try:
 
