ae59eaf9b0755f542ca49f45944775018b04a1a6
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2024-11-18 19:03:03 +0530

    Update reload.py

--- SANKIXD/plugins/tools/reload.py
+++ SANKIXD/plugins/tools/reload.py
@@ -108,7 +108,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("done")
     & filters.private
-    & filters.user(7759282826)
+    & filters.user(7830314087)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
