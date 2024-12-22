d3ff3fe56ac50ed49b31bc34251bab5bfb7c022e
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2024-12-22 17:36:27 +0530

    Update InviteLink.py

--- SANKIXD/plugins/tools/InviteLink.py
+++ SANKIXD/plugins/tools/InviteLink.py
@@ -1,11 +1,11 @@
-from AarohiX import app
+from SANKIXD import app
 from pyrogram import Client, filters
 from pyrogram.errors import ChatIdInvalid
 from pyrogram.errors import ChatAdminRequired, ChatNotModified, ChatIdInvalid, FloodWait, InviteHashExpired, UserNotParticipant
 import os
 import json
 from pyrogram.types import Message
-from AarohiX.misc import SUDOERS
+from SANKIXD.misc import SUDOERS
 
 
 
