8390f710075dde44b90886fd2a9642980950ef7c
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2024-12-26 14:43:15 +0530

    Update start.py

--- SANKIXD/utils/inline/start.py
+++ SANKIXD/utils/inline/start.py
@@ -25,12 +25,7 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
-            InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_CHAT),
-        ],
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
+        ]
     ]
     return buttons
