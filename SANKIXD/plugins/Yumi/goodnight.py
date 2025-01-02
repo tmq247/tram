1d764c5bdeb76cc4628c50cc8d38014d34d812e1
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-01-02 15:03:02 +0700

    Update goodnight.py

--- SANKIXD/plugins/Yumi/goodnight.py
+++ SANKIXD/plugins/Yumi/goodnight.py
@@ -9,7 +9,7 @@ from SANKIXD import app
 
 
 # "/gn" command ka handler
-@app.on_message(filters.command("oodnight", prefixes="g"))
+@app.on_message(filters.command("gủ", prefixes="n"))
 def goodnight_command_handler(client: Client, message: Message):
     # Randomly decide whether to send a sticker or an emoji
     send_sticker = random.choice([True, False])
