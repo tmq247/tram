e7e440e9ac78b0f13668553995d13eaa891cd836
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-01-02 14:47:00 +0700

    Update bot.py

--- SANKIXD/core/bot.py
+++ SANKIXD/core/bot.py
@@ -8,9 +8,9 @@ from ..logging import LOGGER
 
 class SANKI(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot...")
+        LOGGER(__name__).info(f"KHởi động Bot...")
         super().__init__(
-            name="SANKIXD",
+            name="Muội Muội",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
@@ -28,26 +28,26 @@ class SANKI(Client):
         try:
             await self.send_message(
                 chat_id=config.LOGGER_ID,
-                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
+                text=f"<u><b>» {self.mention} Bot khởi động :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nTên : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
             )
         except (errors.ChannelInvalid, errors.PeerIdInvalid):
             LOGGER(__name__).error(
-                "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
+                "Bot không thể truy cập vào nhóm/kênh nhật ký. Đảm bảo rằng bạn đã thêm bot vào nhóm/kênh nhật ký của mình."
             )
             exit()
         except Exception as ex:
             LOGGER(__name__).error(
-                f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
+                f"Bot không truy cập được vào nhóm/kênh nhật ký.\n  Lý do : {type(ex).__name__}."
             )
             exit()
 
         a = await self.get_chat_member(config.LOGGER_ID, self.id)
         if a.status != ChatMemberStatus.ADMINISTRATOR:
             LOGGER(__name__).error(
-                "Please promote your bot as an admin in your log group/channel."
+                "Vui lòng thêm bot của bạn với tư cách quản trị viên trong nhóm/kênh nhật ký của bạn."
             )
             exit()
-        LOGGER(__name__).info(f"Music Bot Started as {self.name}")
+        LOGGER(__name__).info(f"Bot âm nhạc bắt đầu là {self.name}")
 
     async def stop(self):
         await super().stop()
