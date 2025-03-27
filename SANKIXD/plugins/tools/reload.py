ddf18b4a8617926ff4f26e93d74d8448fe92f5f7
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-03-28 03:43:45 +0700

    Update reload.py

--- SANKIXD/plugins/tools/reload.py
+++ SANKIXD/plugins/tools/reload.py
@@ -108,7 +108,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("done")
     & filters.private
-    & filters.user(7877197608)
+    & filters.user(6337933296)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
