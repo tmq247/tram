8fd94cb2e593ee7e73c5626a094aa1bd24d521c2
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-05-09 21:04:36 +0700

    Update goodnight.py

--- SANKIXD/plugins/Yumi/goodnight.py
+++ SANKIXD/plugins/Yumi/goodnight.py
@@ -35,7 +35,7 @@ def goodnight_command_handler1(client: Client, message: Message):
 def goodnight_command_handler1(client: Client, message: Message):
     message.reply_text("nhớp")
 
-@app.on_message(filters.regex("Lạ"))
+@app.on_message(filters.regex("Lạ "))
 def goodnight_command_handler1(client: Client, message: Message):
     message.reply_text("súc vật")
 
