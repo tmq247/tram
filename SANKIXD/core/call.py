d48bff696cfa9a54063ee4358fe0956a9e24f9e7
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-05-14 00:19:21 +0700

    Update call.py

--- SANKIXD/core/call.py
+++ SANKIXD/core/call.py
@@ -6,7 +6,7 @@ from typing import Union
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup
 from pytgcalls import PyTgCalls, StreamType
-from ntgcalls import StreamType
+#from ntgcalls import StreamType
 from pytgcalls.exceptions import (
     AlreadyJoinedError,
     NoActiveGroupCall,
