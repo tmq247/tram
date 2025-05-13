a5273ce9995c7f4b22994392f0ce99ef307c32d2
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-05-14 05:35:37 +0700

    Update call.py

--- SANKIXD/core/call.py
+++ SANKIXD/core/call.py
@@ -601,11 +601,11 @@ class Call(PyTgCalls):
         async def stream_services_handler(_, chat_id: int):
             await self.stop_stream(chat_id)
 
-        @self.one.on_stream_end()
-        @self.two.on_stream_end()
-        @self.three.on_stream_end()
-        @self.four.on_stream_end()
-        @self.five.on_stream_end()
+       # @self.one.on_stream_end()
+       # @self.two.on_stream_end()
+       # @self.three.on_stream_end()
+      #  @self.four.on_stream_end()
+       # @self.five.on_stream_end()
         async def stream_end_handler1(client, update: Update):
             if not isinstance(update, StreamAudioEnded):
                 return
