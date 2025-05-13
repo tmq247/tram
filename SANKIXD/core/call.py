e550593433bb4caf4021b5d30440949a868a883c
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-05-14 01:59:07 +0700

    Update call.py

--- SANKIXD/core/call.py
+++ SANKIXD/core/call.py
@@ -6,7 +6,6 @@ from typing import Union
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup
 from pytgcalls import PyTgCalls
-from pytgcalls.exceptions import AlreadyJoinedError, NoActiveGroupCall, TelegramServerError
 from pytgcalls.types import Update
 from pytgcalls.types.stream import StreamAudioEnded
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
