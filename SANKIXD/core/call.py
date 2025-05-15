888d500bf773eab8084e46fc69213f672398b928
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-05-16 03:02:26 +0700

    Update call.py

--- SANKIXD/core/call.py
+++ SANKIXD/core/call.py
@@ -4,14 +4,39 @@ from datetime import datetime, timedelta
 from typing import Union
 
 from pyrogram import Client
+from pyrogram.types import InlineKeyboardMarkup
 from pytgcalls import PyTgCalls, StreamType
+from pytgcalls.exceptions import (
+    AlreadyJoinedError,
+    NoActiveGroupCall,
+    TelegramServerError,
+)
+from pytgcalls.types import Update
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
 from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityVideo
+from pytgcalls.types.stream import StreamAudioEnded
 
 import config
 from SANKIXD import LOGGER, YouTube, app
-from SANKIXD.utils.database import add_active_chat, remove_active_chat
+from SANKIXD.misc import db
+from SANKIXD.utils.database import (
+    add_active_chat,
+    add_active_video_chat,
+    get_lang,
+    get_loop,
+    group_assistant,
+    is_autoend,
+    music_on,
+    remove_active_chat,
+    remove_active_video_chat,
+    set_loop,
+)
 from SANKIXD.utils.exceptions import AssistantErr
+from SANKIXD.utils.formatters import check_duration, seconds_to_min, speed_converter
+from SANKIXD.utils.inline.play import stream_markup
+from SANKIXD.utils.stream.autoclear import auto_clean
+from SANKIXD.utils.thumbnails import get_thumb
+from strings import get_string
 
 class Call(PyTgCalls):
     def __init__(self):
@@ -21,7 +46,8 @@ class Call(PyTgCalls):
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
         )
-        self.one = PyTgCalls(self.userbot1)
+        self.one = PyTgCalls(self.userbot1, cache_duration=100)
+        self.autoend = {}  # Khai báo biến autoend trong lớp
 
     async def stop_stream(self, chat_id: int):
         assistant = self.one
