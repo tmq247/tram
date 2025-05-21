9b7828405bdb19f2d48b75b60b17b382c641443c
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-05-21 20:02:40 +0700

    Update tts.py

--- SANKIXD/plugins/misc/tts.py
+++ SANKIXD/plugins/misc/tts.py
@@ -6,6 +6,7 @@ from SANKIXD import app
 @app.on_message(filters.command('tts'))
 def text_to_speech(client, message):
     text = message.text.split(' ', 1)[1]
-    tts = gTTS(text=text, lang='hi')
+    tts = gTTS(text=text, lang='vn')
     tts.save('speech.mp3')
     client.send_audio(message.chat.id, 'speech.mp3')
+
