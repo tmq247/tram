7cb3e78a0dd34b28e43a6bf32fee32948a135656
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-05-31 07:55:08 +0700

    Update tts.py

--- SANKIXD/plugins/misc/tts.py
+++ SANKIXD/plugins/misc/tts.py
@@ -1,14 +1,4 @@
-"""(from pyrogram import Client, filters
-from gtts import gTTS
-from SANKIXD import app
-
-
-@app.on_message(filters.command('tts'))
-def text_to_speech(client, message):
-    text = message.text.split(' ', 1)[1]
-    tts = gTTS(text=text, lang='vi')
-    tts.save('speech.mp3')
-    client.send_audio(message.chat.id, 'speech.mp3') )"""
+"""
 from pyrogram import Client, filters
 from gtts import gTTS
 from pydub import AudioSegment
@@ -33,4 +23,20 @@ def text_to_speech(client, message):
     
     # Gửi file âm thanh
     client.send_audio(message.chat.id, 'enhanced_speech.mp3')
+"""
+from pyrogram import Client, filters
+from TTS.api import TTS
+from SANKIXD import app
 
+@app.on_message(filters.command('tts'))
+def text_to_speech(client, message):
+    text = message.text.split(' ', 1)[1]
+    
+    # Khởi tạo mô hình TTS
+    tts = TTS("tts_models/vietnamese/vietTTS")
+    
+    # Tạo file âm thanh với giọng tự nhiên
+    tts.tts_to_file(text=text, file_path="speech.mp3")
+
+    # Gửi file âm thanh
+    client.send_audio(message.chat.id, "speech.mp3")
