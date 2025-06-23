792a9a591d5b6a0cedd2b93a9d015452847c55c1
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-06-23 13:40:46 +0700

    Update mongochk.py

--- SANKIXD/plugins/misc/mongochk.py
+++ SANKIXD/plugins/misc/mongochk.py
@@ -8,7 +8,7 @@ from SANKIXD import app as SANKI
 mongo_url_pattern = re.compile(r'mongodb(?:\+srv)?:\/\/[^\s]+')
 
 
-@SANKI.on_message(filters.command("mongochk"))
+@app.on_message(filters.command("mongochk"))
 async def mongo_command(client, message: Message):
     if len(message.command) < 2:
         await message.reply("Please enter your MongoDB URL after the command. Example: /mongochk your_mongodb_url")
