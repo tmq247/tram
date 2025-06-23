cf79e91ebc3f24a42ba420f907a5806d45fbd524
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-06-23 14:39:30 +0700

    Update watcher.py

--- SANKIXD/plugins/misc/watcher.py
+++ SANKIXD/plugins/misc/watcher.py
@@ -4,8 +4,8 @@ from pyrogram.types import Message
 from SANKIXD import app
 from SANKIXD.core.call import SANKI
 
-welcome = 1
-close = 2
+welcome = 20
+close = 30
 
 
 @app.on_message(filters.video_chat_started, group=welcome)
