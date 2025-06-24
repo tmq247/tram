4f7190f24b95e52ee2221c7283971c17cbd6bc6e
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-06-25 03:35:45 +0700

    Update callback.py

--- SANKIXD/plugins/admins/callback.py
+++ SANKIXD/plugins/admins/callback.py
@@ -396,7 +396,7 @@ async def markup_timer():
                     language = await get_lang(chat_id)
                     _ = get_string(language)
                 except:
-                    _ = get_string("en")
+                    _ = get_string("vn")
                 try:
                     buttons = stream_markup_timer(
                         _,
