899704be58b08ffb9012c17fea186018d6de30bc
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-06-24 18:29:23 +0700

    Update group.py

--- SANKIXD/plugins/tools/group.py
+++ SANKIXD/plugins/tools/group.py
@@ -12,11 +12,11 @@ async def brah2(_, msg):
        await msg.reply("**Cuộc gọi nhóm đã tắt**")
 
 # invite members on vc
-@app.on_message(filters.video_chat_members_invited)
+@app.on_message(filters.video_chat_participants_invited_filter)
 async def brah3(app :app, message:Message):
            text = f"{message.from_user.mention} mời "
            x = 0
-           for user in message.video_chat_members_invited.users:
+           for user in message.video_chat_participants_invited_filter.users:
              try:
                text += f"[{user.first_name}](tg://user?id={user.id}) "
                x += 1
