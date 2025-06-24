4721dbf7f614de56e89e1ca2eff2a2eb1f7c94f3
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-06-25 01:41:00 +0700

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -12,11 +12,11 @@ def get_string(lang: str):
 
 
 for filename in os.listdir(r"./strings/langs/"):
-    if "ar" not in languages:
-        languages["ar"] = yaml.safe_load(
+    if "vn" not in languages:
+        languages["vn"] = yaml.safe_load(
             open(r"./strings/langs/en.yml", encoding="utf8")
         )
-        languages_present["ar"] = languages["ar"]["name"]
+        languages_present["vn"] = languages["vn"]["name"]
     if filename.endswith(".yml"):
         language_name = filename[:-4]
         if language_name == "vn":
@@ -24,7 +24,7 @@ for filename in os.listdir(r"./strings/langs/"):
         languages[language_name] = yaml.safe_load(
             open(r"./strings/langs/" + filename, encoding="utf8")
         )
-        for item in languages["ar"]:
+        for item in languages["vn"]:
             if item not in languages[language_name]:
                 languages[language_name][item] = languages["vn"][item]
     try:
