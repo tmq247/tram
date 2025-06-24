74521590650e502a3aad4f94523b9c553526424e
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-06-25 02:45:08 +0700

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,34 +1,32 @@
 import os
-from typing import List
-
 import yaml
+from typing import List
 
 languages = {}
 languages_present = {}
 
-
 def get_string(lang: str):
-    return languages[lang]
+    return languages.get(lang)
 
+lang_dir = r"./strings/langs/"
+with open(os.path.join(lang_dir, "vn.yml"), encoding="utf8") as file:
+    languages["vn"] = yaml.safe_load(file)
+    languages_present["vn"] = languages["vn"]["name"]
 
-for filename in os.listdir(r"./strings/langs/"):
-    if "en" not in languages:
-        languages["en"] = yaml.safe_load(
-            open(r"./strings/langs/en.yml", encoding="utf8")
-        )
-        languages_present["en"] = languages["en"]["name"]
-    if filename.endswith(".yml"):
+for filename in os.listdir(lang_dir):
+    if filename.endswith(".yml") and filename != "vn.yml":
         language_name = filename[:-4]
-        if language_name == "en":
-            continue
-        languages[language_name] = yaml.safe_load(
-            open(r"./strings/langs/" + filename, encoding="utf8")
-        )
-        for item in languages["en"]:
-            if item not in languages[language_name]:
-                languages[language_name][item] = languages["en"][item]
-    try:
-        languages_present[language_name] = languages[language_name]["name"]
-    except:
-        print("There is some issue with the language file inside bot.")
-        exit()
+        file_path = os.path.join(lang_dir, filename)
+        try:
+            with open(file_path, encoding="utf8") as file:
+                languages[language_name] = yaml.safe_load(file)
+            for item in languages["vn"]:
+                if item not in languages[language_name]:
+                    languages[language_name][item] = languages["vn"][item]
+            languages_present[language_name] = languages[language_name]["name"]
+        except KeyError as e:
+            print(f"Missing 'name' key in {filename}. Error: {e}")
+            exit(1)
+        except Exception as e:
+            print(f"Error loading language file {filename}: {e}")
+            exit(1)
