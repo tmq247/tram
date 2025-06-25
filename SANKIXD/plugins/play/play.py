98b7434cb69402062aaeda1725673ee905329356
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-06-26 03:05:21 +0700

    Update play.py

--- SANKIXD/plugins/play/play.py
+++ SANKIXD/plugins/play/play.py
@@ -55,7 +55,7 @@ async def play_commnd(
     user_id = message.from_user.id
     user_name = message.from_user.first_name
     assistant = await group_assistant(SANKI, message.chat.id)
-    if not (group_call := (await get_group_call(assistant, m, err_msg=", ɢʀᴏᴜᴘ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴀʟʀᴇᴀᴅʏ ᴇɴᴅᴇᴅ"))):  
+    if not (group_call := (await get_group_call(assistant, message, err_msg=", ɢʀᴏᴜᴘ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴀʟʀᴇᴀᴅʏ ᴇɴᴅᴇᴅ"))):  
             return 
     audio_telegram = (
         (message.reply_to_message.audio or message.reply_to_message.voice)
