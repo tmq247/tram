f389e27eb9733f3cbba2a3006f6a5047104aac6a
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-06-27 02:41:49 +0700

    Update welcome.py

--- SANKIXD/plugins/tools/welcome.py
+++ SANKIXD/plugins/tools/welcome.py
@@ -113,10 +113,10 @@ async def bot_wel(_, message):
     for u in message.new_chat_members:
        if u.id == app.me.id:
             await app.send_message(LOG_CHANNEL_ID, f"""
-NEW GROUP
+"""(NEW GROUP
 ╭───── • ◆ • ─────╮
   NAME: {message.chat.title}
   ID: {message.chat.id}
   USERNAME: @{message.chat.username}
 ╰───── • ◆ • ─────╯
-""") """
+"""
