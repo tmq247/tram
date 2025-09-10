9df9185d497a110514a44ffed549ff81e8cf628e
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-09-10 22:49:31 +0700

    Update play.py

--- SANKIXD/plugins/play/play.py
+++ SANKIXD/plugins/play/play.py
@@ -69,7 +69,7 @@ async def play_commnd(
         else None
     )
     if audio_telegram:
-        if audio_telegram.file_size > 10485:
+        if audio_telegram.file_size > 10485760000000:
             return await mystic.edit_text(_["play_5"])
         duration_min = seconds_to_min(audio_telegram.duration)
         if (audio_telegram.duration) > config.DURATION_LIMIT:
