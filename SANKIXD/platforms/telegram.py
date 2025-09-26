ca99014498a3b233e6c12d4ec2ecb7a93006d33f
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-09-26 14:19:33 +0700

    Update telegram.py

--- SANKIXD/platforms/telegram.py
+++ SANKIXD/platforms/telegram.py
@@ -18,7 +18,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Voice
 
 import config
 from config import lyrical
-from YukkiMusic import app
+from SANKIXD import app
 
 from ..utils.formatters import convert_bytes, get_readable_time, seconds_to_min
 
