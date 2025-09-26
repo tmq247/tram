640e40b34ed56b28b20cc568fcd523cc87fe6675
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-09-26 15:15:51 +0700

    Update config.py

--- config.py
+++ config.py
@@ -128,7 +128,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "52428800000000"
 # ------------------------------------
 # ------------------------------------
 # ------------------------------------
-STRING1 = getenv("STRING_SESSION", None)
+STRING1 = parse_list(getenv("STRING_SESSIONS", ""))
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
