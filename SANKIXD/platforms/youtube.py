b5d19a4e2e024ad1cde5746887ed734d437fbdd6
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-09-26 14:20:14 +0700

    Update youtube.py

--- SANKIXD/platforms/youtube.py
+++ SANKIXD/platforms/youtube.py
@@ -19,9 +19,9 @@ from yt_dlp import YoutubeDL
 
 import config
 from config import cookies
-from YukkiMusic.utils.database import is_on_off
-from YukkiMusic.utils.decorators import asyncify
-from YukkiMusic.utils.formatters import seconds_to_min, time_to_seconds
+from SANKIXD.utils.database import is_on_off
+from SANKIXD.utils.decorators import asyncify
+from SANKIXD.utils.formatters import seconds_to_min, time_to_seconds
 
 NOTHING = {"cookies_dead": None}
 
