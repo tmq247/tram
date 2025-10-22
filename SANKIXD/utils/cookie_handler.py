92571c6b9624b365fa8dd10570ce56c1482563f2
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-10-22 13:37:47 +0700

    Update cookie_handler.py

--- SANKIXD/utils/cookie_handler.py
+++ SANKIXD/utils/cookie_handler.py
@@ -4,9 +4,9 @@ from pathlib import Path
 from urllib.parse import urlsplit
 
 from config import COOKIE_URL
-from HasiiMusic.utils.errors import capture_internal_err
+from SANKIXD.utils.errors import capture_internal_err
 
-COOKIE_PATH = Path("HasiiMusic/assets/cookies.txt")
+COOKIE_PATH = Path("SANKIXD/cookies/cookies.txt")
 
 
 def _extract_paste_id(url: str) -> str:
