c09c44aebb35d302bb147032287aa7e45c7c221c
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-10-22 12:57:49 +0700

    Update __init__.py

--- SANKIXD/__init__.py
+++ SANKIXD/__init__.py
@@ -18,10 +18,10 @@ userbot = Userbot()
 
 from .platforms import *
 
-Apple = AppleAPI()
-Carbon = CarbonAPI()
-SoundCloud = SoundAPI()
-Spotify = SpotifyAPI()
-Resso = RessoAPI()
+#Apple = AppleAPI()
+#Carbon = CarbonAPI()
+#SoundCloud = SoundAPI()
+#Spotify = SpotifyAPI()
+#Resso = RessoAPI()
 Telegram = TeleAPI()
 YouTube = YouTubeAPI()
