fcc518d4a0d0d84647353b617d0d133eeaf23cb1
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-10-22 13:45:57 +0700

    Update dir.py

--- SANKIXD/core/dir.py
+++ SANKIXD/core/dir.py
@@ -51,3 +51,17 @@ def dirr():
        # os.mkdir("cache")
 
     #LOGGER(__name__).info("Danh mục được cập nhật.")
+
+BASE_DIR = os.getcwd()
+DOWNLOAD_DIR = os.path.join(BASE_DIR, "downloads")
+CACHE_DIR = os.path.join(BASE_DIR, "cache")
+
+def dirr():
+    for file in os.listdir():
+        if file.lower().endswith((".jpg", ".jpeg", ".png")):
+            os.remove(file)
+
+    os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+    os.makedirs(CACHE_DIR, exist_ok=True)
+
+    LOGGER(__name__).info("Directories Updated.")
