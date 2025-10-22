de7b27a93ef5fa632325bd7a7a1ec01d6c364cb6
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-10-22 13:09:01 +0700

    Update start.py

--- SANKIXD/plugins/bot/start.py
+++ SANKIXD/plugins/bot/start.py
@@ -3,7 +3,7 @@ import random
 from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from youtubesearchpython.__future__ import VideosSearch
+from yt_dlp import VideosSearch
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
 import config
