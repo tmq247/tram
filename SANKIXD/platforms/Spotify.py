041a78a5cfd24bd22574a34e10a835e42f687b8a
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-10-22 13:54:28 +0700

    Add files via upload

---
+++ SANKIXD/platforms/Spotify.py
@@ -0,0 +1,94 @@
+import re
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from py_yt  import VideosSearch
+
+import config
+
+
+class SpotifyAPI:
+    def __init__(self):
+        self.regex = r"^https:\/\/open\.spotify\.com\/.+"
+        self.client_id = config.SPOTIFY_CLIENT_ID
+        self.client_secret = config.SPOTIFY_CLIENT_SECRET
+        if self.client_id and self.client_secret:
+            self.client_credentials_manager = SpotifyClientCredentials(
+                self.client_id, self.client_secret
+            )
+            self.spotify = spotipy.Spotify(
+                client_credentials_manager=self.client_credentials_manager
+            )
+        else:
+            self.spotify = None
+
+    async def valid(self, link: str) -> bool:
+        return bool(re.search(self.regex, link or ""))
+
+    async def track(self, link: str):
+        if not self.spotify:
+            raise RuntimeError("Spotify credentials not configured")
+        track = self.spotify.track(link)
+        info = track["name"]
+        for artist in track["artists"]:
+            fetched = f' {artist["name"]}'
+            if "Various Artists" not in fetched:
+                info += fetched
+        results = VideosSearch(info, limit=1)
+        data = await results.next()
+        r = data["result"][0]
+        track_details = {
+            "title": r["title"],
+            "link": r["link"],
+            "vidid": r["id"],
+            "duration_min": r["duration"],
+            "thumb": r["thumbnails"][0]["url"].split("?")[0],
+        }
+        return track_details, track_details["vidid"]
+
+    async def playlist(self, url):
+        if not self.spotify:
+            raise RuntimeError("Spotify credentials not configured")
+        playlist = self.spotify.playlist(url)
+        playlist_id = playlist["id"]
+        results = []
+        for item in playlist["tracks"]["items"]:
+            music_track = item["track"]
+            info = music_track["name"]
+            for artist in music_track["artists"]:
+                fetched = f' {artist["name"]}'
+                if "Various Artists" not in fetched:
+                    info += fetched
+            results.append(info)
+        return results, playlist_id
+
+    async def album(self, url):
+        if not self.spotify:
+            raise RuntimeError("Spotify credentials not configured")
+        album = self.spotify.album(url)
+        album_id = album["id"]
+        results = []
+        for item in album["tracks"]["items"]:
+            info = item["name"]
+            for artist in item["artists"]:
+                fetched = f' {artist["name"]}'
+                if "Various Artists" not in fetched:
+                    info += fetched
+            results.append(info)
+        return results, album_id
+
+    async def artist(self, url):
+        if not self.spotify:
+            raise RuntimeError("Spotify credentials not configured")
+        artistinfo = self.spotify.artist(url)
+        artist_id = artistinfo["id"]
+        results = []
+        artisttoptracks = self.spotify.artist_top_tracks(url)
+        for item in artisttoptracks["tracks"]:
+            info = item["name"]
+            for artist in item["artists"]:
+                fetched = f' {artist["name"]}'
+                if "Various Artists" not in fetched:
+                    info += fetched
+            results.append(info)
+        return results, artist_id
